17af2027142e126002a27f9611601d8965836b7e
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2024-02-07 15:09:15 +0530

    Update OAuth.jsx
    navigate change

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -27,7 +27,7 @@ export default function OAuth() {
             const data = await res.json()
             if (res.ok){
                 dispatch(signInSuccess(data))
-                navigate('/');
+                navigate('/dashboard?tab=dash');
             }
         } catch (error) {
             console.log(error);
